31859cefc5b501295c6ab4b7864f341648f0dbb7
thomas434 <thomas@dokkanz.com>
2020-05-11 18:47:53 +0200

    configure project

--- manage.py
+++ manage.py
@@ -4,8 +4,8 @@ import os
 import sys
 
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'microworkers.settings')
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'microworkers.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,6 +16,3 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
-    main()
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+#! URL Configuration
+from django.urls import path, include
+from . import views
+
+
+
+urlpatterns = [
+    
+]
+
+
+
