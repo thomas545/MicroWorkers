82d55bec8d181b7ff324be67f2937cafd51d6931
thomas434 <thomas@dokkanz.com>
2020-05-24 14:48:20 +0200

    add payment app

--- microworkers/settings/development.py
+++ microworkers/settings/development.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # Applications
     'users',
     'tasks',
+    'payment',
 
 ]
 
---
+++ payment/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from base.models import TimeStampedModel
+
+
+class Transaction(TimeStampedModel):
+    pass
+
+
+
+class Claim(TimeStampedModel):
+    pass
